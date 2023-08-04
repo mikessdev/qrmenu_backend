57d22779ab6ef5c89815566410ec42147287dfe6
mikessdev <mikesaints.2000@gmail.com>
2023-08-04 12:16:23 -0300

    configuring migrations for product table

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { dataBaseConfig } from './Database/database.providers';
+import { dataBaseConfig } from './database/database.providers';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
