afd9158af015e725b06827604656940b95071719
mikessdev <mikesaints.2000@gmail.com>
2023-08-31 22:37:05 -0300

    refac: Refactoring seeders objects

--- .db/seeders/20230807040621-categories.js
+++ .db/seeders/20230807040621-categories.js
@@ -7,14 +7,38 @@ module.exports = {
       'categories',
       [
         {
-          id: '1',
-          title: 'Porções',
+          id: '0c8822f5-add0-4a68-abed-afa880df5096',
+          title: 'Entradas e Porções',
           updatedAt: new Date(),
           createdAt: new Date(),
         },
         {
-          id: '2',
+          id: '06adf84e-e80c-41c1-8cbe-5899496aa61f',
+          title: 'Prato Principal',
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        },
+        {
+          id: '401053f2-83ef-4dcf-bdc0-579cb57d8b33',
+          title: 'Lanches',
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        },
+        {
+          id: '21df6b7e-bb34-4bc9-8384-5d069d343c88',
+          title: 'Bebidas',
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        },
+        {
+          id: '6f494095-00b2-427e-99b0-a50bee75e0ae',
           title: 'Pasteis',
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        },
+        {
+          id: '39b9f328-04db-4046-b8f3-745884ed0e14',
+          title: 'Coxinhas',
           updatedAt: new Date(),
           createdAt: new Date(),
         },
