0e4f5557f0347557ba26754b981297e25c64f321
mikessdev <mikesaints.2000@gmail.com>
2023-09-02 19:10:31 -0300

    test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,15 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-// import { firebaseAuth } from './firebaseAuth/app.firebase';
-// import { signInWithEmailAndPassword } from '@firebase/auth';
-
-console.log('🚀🚀', process.env.MY_NAME);
-console.log('🚀');
+import { firebaseAuth } from './firebaseAuth/app.firebase';
+import { signInWithEmailAndPassword } from '@firebase/auth';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  // let accessToken: string;
+  let accessToken: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -20,22 +17,20 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
 
-    // const getUser = await signInWithEmailAndPassword(
-    //   firebaseAuth,
-    //   process.env.USER_EMAIL,
-    //   process.env.USER_PASSWORD,
-    // );
+    const getUser = await signInWithEmailAndPassword(
+      firebaseAuth,
+      process.env.USER_EMAIL,
+      process.env.USER_PASSWORD,
+    );
 
-    // accessToken = getUser.user['accessToken'];
+    accessToken = getUser.user['accessToken'];
   });
 
   it('/ (GET)', () => {
-    return (
-      request(app.getHttpServer())
-        .get('/')
-        // .set('Authorization', `Bearer ${accessToken}`)
-        .expect(200)
-        .expect('Hello World!')
-    );
+    return request(app.getHttpServer())
+      .get('/')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .expect(200)
+      .expect('Hello World!');
   });
 });
