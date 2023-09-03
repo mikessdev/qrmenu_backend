9bea60de3522909d1691754c65f98cc89cd8fc44
mikessdev <mikesaints.2000@gmail.com>
2023-09-02 21:25:27 -0300

    test

--- test/firebaseAuth/app.firebase.ts
+++ test/firebaseAuth/app.firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import * as core from '@actions/core';
 
 export const firebaseConfig = {
   apiKey: process.env.FIREBASE_APP_API_KEY,
