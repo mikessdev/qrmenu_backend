4ea0a8a3757272aedcb11167264e25fb7c63ce7d
mikessdev <mikess.dev@gmail.com>
2023-09-19 17:26:55 -0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
   await app.listen(3005);
 }
 bootstrap();
