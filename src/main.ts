ab58b008bc10ce8a5facf27a79b829a1fa225f64
mikessdev <mikess.dev@gmail.com>
2023-09-19 11:00:40 -0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
   });
   await app.listen(3005);
