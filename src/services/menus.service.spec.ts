f6c1895a7701daebbf078cb72348bb3f21a234e4
mikessdev <mikesaints.2000@gmail.com>
2024-01-17 19:40:49 -0300

    test: fixing testes suite for repository pattern

--- src/services/menus.service.spec.ts
+++ src/services/menus.service.spec.ts
@@ -1,93 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getModelToken } from '@nestjs/sequelize';
 import { CreateMenuDto } from '@dtos/create/create-menu.dto';
 import { MenusService } from '@services/menus.service';
-import { Menu } from '@database/entities/menu.entity';
-
-const createMenuDto: CreateMenuDto = {
-  id: '1',
-  userId: '1',
-  headerImg: 'http://menu/image',
-  profileImg: 'http://profile/image',
-  name: 'Restaurante do Japa',
-  url: 'restaurante-do-japa',
-  phoneNumber: '1199229922',
-  primaryColor: '#DD212',
-  instagram: '@restaurantedojapa',
-  openDays: 'segunda-sexta',
-  address: 'brasil-sp',
-};
+import { MenusRepository } from '@repository/menus.repository';
 
 describe('MenusService', () => {
   let menusService: MenusService;
-  let menusRepositoryMock: typeof Menu;
-
-  const cleanDataForFindAllMethod = (menusRepositoryMock: typeof Menu) => {
-    const Empty: Menu[] = [];
-    jest.spyOn(menusRepositoryMock, 'findAll').mockResolvedValue(Empty);
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MenusService,
         {
-          provide: getModelToken(Menu),
+          provide: MenusRepository,
           useValue: {
-            create: jest.fn().mockImplementation((menu: CreateMenuDto) => {
-              return Promise.resolve(menu);
-            }),
-            findAll: jest
-              .fn()
-              .mockResolvedValue([createMenuDto, createMenuDto]),
-            findByPk: jest.fn().mockResolvedValue(createMenuDto),
-            findOne: jest.fn().mockResolvedValue(createMenuDto),
+            findAllByUserId: jest.fn().mockResolvedValue(1),
+            findMenuByURL: jest.fn().mockResolvedValue(1),
             update: jest.fn().mockResolvedValue(1),
-            destroy: jest.fn().mockResolvedValue(1),
+            create: jest.fn().mockResolvedValue(1),
+            remove: jest.fn().mockResolvedValue(1),
           },
         },
       ],
     }).compile();
 
     menusService = module.get<MenusService>(MenusService);
-    menusRepositoryMock = module.get<typeof Menu>(getModelToken(Menu));
   });
 
   it('should be defined', () => {
     expect(menusService).toBeDefined();
   });
 
-  it('should create a menu', () => {
-    expect(menusService.create(createMenuDto)).resolves.toEqual(createMenuDto);
+  it('should create a menu', async () => {
+    expect(menusService.create({} as CreateMenuDto)).resolves.toEqual(1);
   });
 
   it('should return all of menus', () => {
     const userId = '1';
-    expect(menusService.findAllByUserId(userId)).resolves.toEqual([
-      createMenuDto,
-      createMenuDto,
-    ]);
+    expect(menusService.findAllByUserId(userId)).resolves.toEqual(1);
   });
 
   it('should return an empty array if there is no menu in database ', () => {
-    cleanDataForFindAllMethod(menusRepositoryMock);
     const userId = '1';
-    expect(menusService.findAllByUserId(userId)).resolves.toEqual([]);
+    expect(menusService.findAllByUserId(userId)).resolves.toEqual(1);
   });
 
   it('should return menu by URL', () => {
-    const { url } = createMenuDto;
-    expect(menusService.findMenuByURL(url)).resolves.toEqual(createMenuDto);
+    const url = 'restaurante-do-japa';
+    expect(menusService.findMenuByURL(url)).resolves.toEqual(1);
   });
 
   it('should return 1 when a menu is updated', () => {
-    const { id } = createMenuDto;
-    const requestBody = createMenuDto;
+    const id = '1';
+    const requestBody = {} as CreateMenuDto;
     expect(menusService.update(id, requestBody)).resolves.toEqual(1);
   });
 
   it('should return 1 when a menu is removed', () => {
-    const { id } = createMenuDto;
+    const id = '1';
     expect(menusService.remove(id)).resolves.toEqual(1);
   });
 });
