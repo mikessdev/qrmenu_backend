8c3168e4a95aa549d1954ae64f231e093b591fbf
mikessdev <mikesaints.2000@gmail.com>
2024-03-31 22:31:30 -0300

    feat: add after create a menu should fill categories and products with fake data

--- src/services/menus.service.ts
+++ src/services/menus.service.ts
@@ -2,12 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { CreateMenuDto } from '@dtos/create/create-menu.dto';
 import { UpdateMenuDto } from '@dtos/update/update-menu.dto';
 import { MenusRepository } from '@repository/menus.repository';
+import { CategoriesService } from './categories.service';
+import { Status } from '@utils/enum/status.enum';
+import { ProductsService } from './products.service';
 
 @Injectable()
 export class MenusService {
-  constructor(private readonly menuRepository: MenusRepository) {}
+  constructor(
+    private readonly menuRepository: MenusRepository,
+    private readonly categoriesService: CategoriesService,
+    private readonly productsService: ProductsService,
+  ) {}
   async create(createMenuDto: CreateMenuDto) {
-    return await this.menuRepository.create(createMenuDto);
+    const resultMenu = await this.menuRepository.create(createMenuDto);
+
+    if (resultMenu.status === Status.FAILED) {
+      return resultMenu;
+    }
+
+    const resultCategories = await this.categoriesService.createAll(
+      resultMenu.message.id,
+    );
+
+    if (resultCategories.status === Status.FAILED) {
+      return resultMenu;
+    }
+
+    resultCategories.message.forEach(async (category) => {
+      const categoryId = category.dataValues.id;
+      await this.productsService.createAll(categoryId);
+    });
+
+    return resultMenu;
   }
 
   async findAllByUserId(userId: string) {
