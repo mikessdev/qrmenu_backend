3d4f5891641b8244402ba8f3aa5e5798dc2e0dc5
mikessdev <mikesaints.2000@gmail.com>
2024-03-31 22:28:27 -0300

    feat: import category and product modules

--- src/modules/menus.module.ts
+++ src/modules/menus.module.ts
@@ -4,9 +4,15 @@ import { MenusController } from '@controllers/menus.controller';
 import { Menu } from '@database/entities/menu.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { MenusRepository } from '@repository/menus.repository';
+import { CategoriesModule } from './categories.module';
+import { ProductsModule } from './products.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Menu])],
+  imports: [
+    SequelizeModule.forFeature([Menu]),
+    CategoriesModule,
+    ProductsModule,
+  ],
   controllers: [MenusController],
   providers: [MenusService, MenusRepository],
 })
