21ba8f3b30adcde75c7c12d2e9a53580f0531bc4
mikessdev <mikesaints.2000@gmail.com>
2024-11-24 16:20:41 -0300

    feat: add findOneByFirebaseId user endpoint

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -49,10 +49,6 @@ export class AppModule implements NestModule {
           method: RequestMethod.GET,
         },
         {
-          path: 'users/(.*)',
-          method: RequestMethod.GET,
-        },
-        {
           path: 'menus/url/(.*)',
           method: RequestMethod.GET,
         },
